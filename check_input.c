68f9cc1bd171a35a653ece9192804c9c3177872f
Simon Kunert <skunert@1-E-15.42heilbronn.de>
2023-06-12 14:21:26 +0200

    input checking done

--- check_input.c
+++ check_input.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 12:31:17 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/12 13:52:57 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/12 14:19:26 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,44 @@ int	get_len_matrix(char **matrix)
 	return (i);
 }
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	check_type(char **input)
+{
+	int	i;
+
+	i = 0;
+	while (input[i] != NULL)
+	{
+		if (input[i][0] < '1' || input[i][0] > '4')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	check_input(char **input)
 {
+	int	i;
+
+	i = 0;
 	if (get_len_matrix(input) != 16)
-		return (-1);
-	return (0);
+		return (0);
+	while (input[i] != NULL)
+	{
+		if (ft_strlen(input[i]) != 1)
+			return (0);
+		i++;
+	}
+	if (check_type(input) == 0)
+		return (0);
+	return (1);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 12:25:02 by skunert           #+#    #+#             */
-/*   Updated: 2023/06/12 13:54:34 by skunert          ###   ########.fr       */
+/*   Updated: 2023/06/12 14:20:10 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 	{
 		input = ft_split(argv[1], ' ');
-		if (check_input(input) == -1)
-			printf("Wrong\n");
+		if (check_input(input) == 0)
+		{
+			write(2, "Your input is incorrect!\n", 25);
+			return (-1);
+		}
+		free_matrix(input);
 	}
 	else
 		printf("Wrong amount of arguments\n");
